a4194282da8edc15a818f557a09931079f9a35e2
vit017 <kaluzhskikh.v@gmail.com>
2017-02-16 15:49:25 +0400

    active 0

--- City/init.php
+++ City/init.php
@@ -5,11 +5,11 @@ use City\Module as City;
 
 try {
     City::load_settings();
-    City::get_iblocks();
     if (!City::active()) {
         City::disable();
         return;
     }
+    City::get_iblocks();
     City::init();
 } catch (Exception $e) {
     dd('internal', 0);
